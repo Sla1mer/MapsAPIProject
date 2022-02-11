7bd01238c6e786577883ff5a886dd339c5b16a2d
Andrulik <94921596+Prettywoman13@users.noreply.github.com>
2022-02-11 18:54:14 +0200

    добавил две функции

---
+++ get_coord_by_name.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def get_coord(name):
+    API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
+    request_to_get_coord = f"http://geocode-maps.yandex.ru/1.x/?apikey={API_KEY}&geocode={name}&format=json"
+    coord_respone = requests.get(request_to_get_coord)
+    json_coord_respone = coord_respone.json()
+    answ = json_coord_respone["response"]["GeoObjectCollection"]["featureMember"]\
+        [0]["GeoObject"]["boundedBy"]["Envelope"]["lowerCorner"]
+    return tuple(answ.split())
+
+print(get_coord('карамзина 38 88'))
---
+++ get_map_img.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def get_map_image(*all_params):
+    base_request = f"http://static-maps.yandex.ru/1.x/?"
+    for i in all_params:
+        base_request += f"&{i}"
+    image = requests.get(base_request).content
+    return image
+
+# тестовый запрос
+# get_map_image('l=map', 'pt=37.560161,55.791477~37.644923,55.701981')
