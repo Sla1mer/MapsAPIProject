1902b692dc815025a92cdbf0e5d989728c962d65
Prettywoman13 <AveSvecha@yandex.ru>
2022-02-11 19:50:43 +0200

    add new func

--- Models/Variables.py
+++ Models/Variables.py
@@ -2,3 +2,8 @@ from Controllers.get_coord_by_name import get_coord
 
 coord = get_coord("ул. Гайдара, 6, Калининград, Россия")
 spn = 0.003
+
+
+def change_spoon(value: float):
+    global spn
+    spn += value
